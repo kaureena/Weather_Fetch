483cedb39a0783bf52f23dfbbc7e2a3ba917cdef
Reena <chauhanreenabahen1990@gmail.com>
2025-07-15 13:17:05 +0100

    Refctore : load API key from .env  using dotenv

--- fetch_weather.py
+++ fetch_weather.py
@@ -1,6 +1,20 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-def fetch_weather_by_coords(lat: float, lon: float, api_key: str) -> dict:
+# with open ("C:/Users/chauh/OneDrive/Daily Work/Weather_cach/key.env") as f:
+#     print(f.read())
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(BASE_DIR, "key.env")
+load_dotenv(dotenv_path=dotenv_path)
+
+API_KEY = os.getenv("API_KEY")
+print("Loaded API_KEY:", API_KEY)
+
+def fetch_weather_by_coords(lat: float, lon: float, api_key=None):
+    if not api_key:
+        api_key = os.getenv("API_KEY")
+
     url = f'https://api.agromonitoring.com/agro/1.0/weather?lat={lat}&lon={lon}&appid={api_key}'
     response = requests.get(url)
 
@@ -16,9 +30,13 @@ def fetch_weather_by_coords(lat: float, lon: float, api_key: str) -> dict:
 
 
 # Optional manual run (for testing)
+# if __name__ == "__main__":
+#     API_KEY = '823109c6390b5e06e2169d638805ac6e'
+#     lat = 35
+#     lon = 139
+#     result = fetch_weather_by_coords(lat, lon, API_KEY)
+#     print(result)
+
 if __name__ == "__main__":
-    API_KEY = '823109c6390b5e06e2169d638805ac6e'
-    lat = 35
-    lon = 139
-    result = fetch_weather_by_coords(lat, lon, API_KEY)
+    result = fetch_weather_by_coords(35, 139)
     print(result)
--- test_fetch_weather.py
+++ test_fetch_weather.py
@@ -1,9 +1,16 @@
 import unittest
 from fetch_weather import fetch_weather_by_coords
 
+
 class TestWeatherFetcher(unittest.TestCase):
+    def test_valid_api_key(self):
+        result = fetch_weather_by_coords(35,139)
+        print("Test Result",result)
+        self.assertIn("temperature", result)
+
     def test_invalid_api_key(self):
-        result = fetch_weather_by_coords(35, 139, "fake_api_key")
+        result = fetch_weather_by_coords(35, 139, "invalid_key_123")
+        print("Test Result (invalid):", result)
         self.assertIn("error", result)
 
 if __name__ == "__main__":
